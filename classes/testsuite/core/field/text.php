d08ad3016d4235f67fa3f67c435b2e6288736a6e
mtou <mtou@charougna.com>
2011-06-27 11:20:14 +0200

    Added text field type

---
+++ classes/testsuite/core/field/text.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Declares TestSuite_Core_Field_Text
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteCore
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/core/field/text.php
+ * @since     2011-06-24
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides TestSuite_Core_Field_Text
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteCore
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/core/field/text.php
+ */
+abstract class TestSuite_Core_Field_Text extends TestSuite_Field
+{
+
+  /**
+   * Adds default constraints
+   *
+   * @param string $alias alias of the field
+   *
+   * @return TestSuite_Field initialized field
+   */
+  public function __construct($alias)
+  {
+    parent::__construct($alias);
+
+    $this->add(TestSuite_Constraint::factory('required', FALSE));
+    $this->add(TestSuite_Constraint::factory('unique', FALSE));
+  }
+
+} // End class TestSuite_Core_Field_Text
