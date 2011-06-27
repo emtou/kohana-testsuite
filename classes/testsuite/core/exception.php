984068b9a59f56c7aecedb304475b6833018b308
mtou <mtou@charougna.com>
2011-06-27 09:42:21 +0200

    Added TestSuite_Core_Exception

---
+++ classes/testsuite/core/exception.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares TestSuite_Core_Exception
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteCore
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/core/exception.php
+ * @since     2011-06-24
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides TestSuite_Core_Exception
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteCore
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/core/exception.php
+ */
+abstract class TestSuite_Core_Exception extends Kohana_Exception
+{
+
+} // End class TestSuite_Core_Exception
