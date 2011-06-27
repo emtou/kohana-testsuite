40bc7ae684918ecaf2e3f913c6dd0cbf7b417560
mtou <mtou@charougna.com>
2011-06-27 11:07:57 +0200

    Added timestamp field type

---
+++ classes/testsuite/field/timestamp.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares TestSuite_Field_Timestamp
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteField
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/field/timestamp.php
+ * @since     2011-06-24
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides TestSuite_Field_Timestamp
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteField
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/field/timestamp.php
+ */
+class TestSuite_Field_Timestamp extends TestSuite_Core_Field_Timestamp
+{
+
+} // End class TestSuite_Field_Timestamp
