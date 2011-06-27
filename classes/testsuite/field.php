d209c6ba83d1f9ae12f3374d3559a0ee4498eadd
mtou <mtou@charougna.com>
2011-06-27 10:09:56 +0200

    Added field management

---
+++ classes/testsuite/field.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares TestSuite_Field
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteField
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/field.php
+ * @since     2011-06-24
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides TestSuite_Field
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteField
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/field.php
+ */
+abstract class TestSuite_Field extends TestSuite_Core_Field
+{
+
+} // End class TestSuite_Field
