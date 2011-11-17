4a24172d3f9e6d1e7ce1eb93c75d44f81e289c39
mtou <mtou@charougna.com>
2011-11-17 15:01:35 +0100

    Add ACL action testing

---
+++ classes/testsuite/controller/action/acl.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Declares TestSuite_Controller_Action_ACL
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteController
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/controller/action/acl.php
+ * @since     2011-11-17
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides TestSuite_Controller_Action_ACL
+ *
+ * PHP version 5
+ *
+ * @group testsuite
+ *
+ * @category  TestSuite
+ * @package   TestSuiteController
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-testsuite/tree/master/classes/testsuite/controller/action/acl.php
+ */
+abstract class TestSuite_Controller_Action_ACL extends TestSuite_Core_Controller_Action_ACL
+{
+
+} // End class TestSuite_Controller_Action_ACL
